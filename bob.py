a393ae5e379e3416bed0648aa82a7163e92f64be
David Hoppenbrouwers <david@ulyssis.org>
2018-11-27 23:41:33 +0100

    Added events

--- bob.py
+++ bob.py
@@ -17,6 +17,21 @@ import os
 from time import sleep
 
 
+
+def parse_time(time):
+    d = {'w': 0, 'd': 0, 'h': 0, 'm': 0, 's': 0}
+    i, j = 0, 0
+    while len(time) != i:
+        if time[i] == '-':
+            i += 1
+        while time[i].isdecimal():
+            i += 1
+        d[time[i]] = int(time[j:i])
+        i += 1
+        j = i
+    return timedelta(weeks=d['w'], days=d['d'], hours=d['h'], minutes=d['m'], seconds=d['s'])
+
+
 class Tell():
 
     def __init__(self, author, recipient, body):
@@ -52,6 +67,60 @@ class Reminder():
     def deserialize(d):
         return Reminder(datetime.fromisoformat(d[0]), d[1])
 
+
+class Event(Reminder):
+    
+    def __init__(self, time, body):
+        super().__init__(time, body)
+        self.uids = []
+        self.resetDue()
+
+    def add_user(self, uid):
+        if uid not in self.uids:
+            self.uids.append(uid)
+            return True
+        return False
+
+    def remove_user(self, uid):
+        if uid in self.user_ids:
+            self.uids.remove(uid)
+            return True
+        return False
+
+    def resetDue(self):
+        now = datetime.now()
+        delta = self.time - now
+        if delta.days >= 7:
+            delta.days = delta.days // 7 * 7
+            delta.hours = self.time.hours
+            delta.minutes = self.time.minutes
+            delta.seconds = 0
+            delta.microseconds = 0
+        elif delta.days >= 1:
+            delta = timedelta(days = 1)
+        elif delta.seconds > 12 * 3600:
+            delta = timedelta(hours = 12)
+        elif delta.seconds > 3600:
+            delta = timedelta(hours = 1)
+        else:
+            delta = timedelta(hours = 0)
+        self.due = self.time - delta
+
+    def isReminderDue(self):
+        return datetime.now() >= self.due
+
+    def hasbegun(self):
+        return datetime.now() >= self.time
+
+    def serialize(self):
+        return [self.time.isoformat(), self.body, self.uids]
+
+    def deserialize(d):
+        e = Event(datetime.fromisoformat(d[0]), d[1])
+        e.uids = d[2]
+        return e
+
+
 class Group():
 
     def __init__(self, client, id):
@@ -81,6 +150,7 @@ class Group():
             except FileNotFoundError:
                 with open(path, 'w') as f:
                     f.write('[]')
+
         self.reminders = []
         try:
             with open('groups/%d/reminders' % self.id, 'r') as f:
@@ -89,6 +159,14 @@ class Group():
                     self.reminders.append(Reminder.deserialize(i))
         except FileNotFoundError:
             with open('groups/%d/reminders' % self.id, 'w') as f:
+                f.write('[]')
+
+        self.events = []
+        try:
+            with open('groups/%d/events' % self.id, 'r') as f:
+                self.events = [Event.deserialize(e) for e in literal_eval(f.read())]
+        except FileNotFoundError:
+            with open('groups/%d/events' % self.id, 'w') as f:
                 f.write('[]')
 
     def add_tell(self, author_id, recipient, message):
@@ -106,15 +184,7 @@ class Group():
         return t
 
     def add_reminder(self, time, body, now):
-        d = {'w': 0, 'd': 0, 'h': 0, 'm': 0, 's': 0}
-        i, j = 0, 0
-        while len(time) != i:
-            while time[i].isdecimal():
-                i += 1
-            d[time[i]] = int(time[j:i])
-            i += 1
-            j = i
-        time = now + timedelta(weeks=d['w'], days=d['d'], hours=d['h'], minutes=d['m'], seconds=d['s'])
+        time = now + parse_time(time)
         self.reminders.append(Reminder(time, body))
         with open('groups/%d/reminders' % self.id, 'w') as f:
             f.write(str([r.serialize() for r in self.reminders]))
@@ -129,6 +199,34 @@ class Group():
         with open('groups/%d/reminders' % self.id, 'w') as f:
             f.write(str([r.serialize() for r in self.reminders]))
         return rem
+
+    def add_event(self, time, hour, minute, body):
+        delta = parse_time(time)
+        delta = timedelta(days=delta.days, hours=int(hour), minutes=int(minute))
+        now = datetime.now()
+        now = datetime(now.year, now.month, now.day)
+        time = now + delta
+        self.events.append(Event(time, body))
+        self.save_events()
+
+    def get_events(self):
+        events = []
+        for e in self.events:
+            if e.isReminderDue():
+                events.append(e)
+                e.resetDue()
+                if e.hasbegun():
+                    self.events.remove(e)
+        self.save_events()
+        return events
+
+    def save_events(self):
+        with open('groups/%d/events' % self.id, 'w') as f:
+            f.write(str([r.serialize() for r in self.events]))
+        
+
+    def get_name(self, uid):
+        return self.names[uid]
 
 
 class Bob(Client):
@@ -146,10 +244,22 @@ class Bob(Client):
     """ Daemons """
     def send_reminders(self):
         while True:
-            for i in self.groups:
-                g = self.groups[i]
-                for r in g.get_reminders():
-                    self.send(Message(text=str(r)), thread_id=g.id, thread_type=ThreadType.GROUP)
+            try:
+                for i in self.groups:
+                    g = self.groups[i]
+                    for r in g.get_reminders():
+                        self.send(Message(text=str(r)), thread_id=g.id, thread_type=ThreadType.GROUP)
+                    for e in g.get_events():
+                        if e.hasbegun():
+                            msg = 'Event \'%s\' has started' % e.body
+                        else:
+                            msg = 'Upcoming event: %s\n\nWhen: %s' % (e.body, str(e.time))
+                        self.send(Message(text=msg), thread_id=g.id, thread_type=ThreadType.GROUP)
+            except Exception as e:
+                print('Reminder daemon crashed! Events and reminders may be lost')
+                print(e)
+                print('Sleeping 30 seconds to prevent excessive log spam')
+                sleep(30)
             sleep(1)
 
     """ General """
@@ -198,6 +308,40 @@ class Bob(Client):
             now = datetime.now()
             time, body = body.split(' ', 1)
             self.get_group(thread_id).add_reminder(time, body, now)
+
+        elif cmd == 'event':
+            g = self.get_group(thread_id)
+            try:
+                cmd, body = body.split(' ', 1)
+            except ValueError:
+                if body == 'list':
+                    if len(g.events) == 0:
+                        return Message(text='No events')
+                    l = ['%d: %s @ %s (%d participants)' % (i, e.body, str(e.time), len(e.uids)) for i,e in enumerate(g.events)]
+                    return Message(text='\n'.join(l))
+                return Message(text='Usage: event <add|list|info|join|leave> <event index...>')
+            if cmd == 'add':
+                time, h_and_m, body = body.split(' ', 2)
+                hour, minute = h_and_m.split(':', 1)
+                g.add_event(time, hour, minute, body)
+                return Message(text='Added event \'%s\'' % body)
+            elif cmd == 'info':
+                i = int(body)
+                e = g.events[i]
+                msg = '%s (ID: %d)\nWhen: %s\nParticipants: %s' % (e.body, i, str(e.time), ', '.join([g.get_name(u) for u in e.uids]))
+                return Message(text=msg)
+            elif cmd == 'join':
+                i = int(body)
+                e = g.events[i]
+                msg = ('Joined event \'%s\'' % e.body) if e.add_user(author_id) else ('Already joined \'%s\'' % e.body)
+                g.save_events()
+                return Message(text=msg)
+            elif cmd == 'leave':
+                i = int(body)
+                e = g.events[i]
+                msg = ('Left event \'%s\'' % e.body) if e.remove_user(author_id) else ('Already left or not joined \'%s\'' % e.body)
+                g.save_events()
+                return Message(text=msg)
 
         elif cmd == 'debug':
             if body == 'users':
